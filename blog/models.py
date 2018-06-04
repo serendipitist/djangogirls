ffc94a03d639908442c487da5951c31c8d5cca35
serendipitist <ps.shireesha@gmail.com>
2018-06-04 19:46:31 +0530

    add the template

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,10 @@
 
-# Create your models here.
-# Create your models here.
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
